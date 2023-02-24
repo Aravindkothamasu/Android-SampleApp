1d590162f747cb11dbd9f1f12bd2670f3c888639
Aravind Kothamasu <aravind.kothamasu@smartm.com>
2023-02-24 18:58:59 +0530

    Capturing default date and time for non chicking the calender

--- app/src/main/java/com/example/sampletestapp/DatabaseActivity.java
+++ app/src/main/java/com/example/sampletestapp/DatabaseActivity.java
@@ -9,13 +9,27 @@ import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
 
+class CurrentDate {
+    int DayOfMonth;
+    int Month;
+    int Year;
+    CurrentDate( int dd, int mm, int yr)
+    {
+        DayOfMonth = dd;
+        Month      = mm;
+        Year       = yr;
+    }
+}
 public class DatabaseActivity extends AppCompatActivity {
     private EditText courseNameEdt, courseTracksEdt, courseDurationEdt, courseDescriptionEdt;
     private Button addCourseBtn;
     private DBHandler dbHandler;
     private CalendarView calendarDate;
-    private int Year, Month, DayOfMonth;
+    CurrentDate date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +37,8 @@ public class DatabaseActivity extends AppCompatActivity {
         setContentView(R.layout.activity_database);
 
         Log.e( getString(R.string.DB), "OnCreate Called");
-
         // initializing all our variables.
+        date                 = new CurrentDate(0, 0, 0);
         courseNameEdt        = findViewById(R.id.idEdtCoureName);
         courseTracksEdt      = findViewById(R.id.idEdtCourseTracks);
         courseDurationEdt    = findViewById(R.id.idEdtCourseDuration);
@@ -43,9 +57,9 @@ public class DatabaseActivity extends AppCompatActivity {
                 // display the selected date by using a toast
                 Toast.makeText(getApplicationContext(), dayOfMonth + "/" + month + 1 + "/" + year, Toast.LENGTH_LONG).show();
                 Log.e(getString(R.string.DB), dayOfMonth + "/" + month + 1 + "/" + year);
-                Year  = year;
-                Month = month+1;
-                DayOfMonth = dayOfMonth;
+                date.Year  = year;
+                date.Month = month+1;
+                date.DayOfMonth = dayOfMonth;
             }
         });
         // below line is to add on click listener for our add course button.
@@ -58,15 +72,20 @@ public class DatabaseActivity extends AppCompatActivity {
                 String courseDuration = courseDurationEdt.getText().toString();
                 String courseDescription = courseDescriptionEdt.getText().toString();
 
+
+                if( date.Year == 0 ) {
+                    date = GetCurrDate(calendarDate);
+                }
                 // validating if the text fields are empty or not.
-                if (courseName.isEmpty() && courseTracks.isEmpty() && courseDuration.isEmpty() && courseDescription.isEmpty()) {
+                if (courseName.isEmpty() && courseTracks.isEmpty() && courseDuration.isEmpty() && courseDescription.isEmpty() && date.Year != 0) {
                     Toast.makeText(DatabaseActivity.this, "Please enter all the data..", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
                 // on below line we are calling a method to add new
                 // course to sqlite data and pass all our values to it.
-                Log.e(getString(R.string.DB), DayOfMonth + "/" + Month + "/" + Year);
+                Log.e(getString(R.string.DB), date.DayOfMonth + "/" + date.Month + "/" + date.Year);
+
                 dbHandler.addNewCourse(courseName, courseDuration, courseDescription, courseTracks, 20);
                 // after adding the data we are displaying a toast message.
                 Toast.makeText(DatabaseActivity.this, "Course has been added.", Toast.LENGTH_SHORT).show();
@@ -74,9 +93,27 @@ public class DatabaseActivity extends AppCompatActivity {
                 courseDurationEdt.setText("");
                 courseTracksEdt.setText("");
                 courseDescriptionEdt.setText("");
-                DayOfMonth = Month = Year = 0;
                 calendarDate.setDate(calendarDate.getDate());
+                date.Month = date.DayOfMonth = date.Year = 0;
             }
         });
+    }
+
+    static CurrentDate GetCurrDate(CalendarView calendarDate ) {
+        long selectedDate;
+        DateFormat format;
+        int dd, mm, yr;
+
+        selectedDate = calendarDate.getDate();
+        format = new SimpleDateFormat("dd");
+        dd = Integer.parseInt(format.format(selectedDate));
+
+        format = new SimpleDateFormat("MM");
+        mm = Integer.parseInt(format.format(selectedDate));
+
+        format = new SimpleDateFormat("YYYY");
+        yr = Integer.parseInt(format.format(selectedDate));
+
+        return new CurrentDate(dd, mm, yr);
     }
 }
