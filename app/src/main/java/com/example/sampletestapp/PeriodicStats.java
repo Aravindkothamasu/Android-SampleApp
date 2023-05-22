3440b815e6f83f137e8e10572eb42ca70de1cdc1
Aravind Kothamasu <aravind.kothamasu@smartm.com>
2023-05-22 16:12:39 +0530

    Added new files for reading Periodic Stats

---
+++ app/src/main/java/com/example/sampletestapp/PeriodicStats.java
@@ -0,0 +1,14 @@
+package com.example.sampletestapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PeriodicStats extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_periodic_stats);
+    }
+}
